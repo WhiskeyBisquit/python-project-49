bada25432a5d90acec75211203795938a89c7897
Egor Zimin <ezimine@gmail.com>
2024-07-26 16:48:29 +0700

    update brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from brain_games.engine import play_game
+from brain_games.games.calc import description, math_problem
+
+
+def start_game():
+    play_game(description, math_problem)
+
+
+def main():
+    start_game()
+
+
+if __name__ == '__main__':
+    main()
