d7ce797ddeafa02879bdf53333d69ec437e3ed2d
Egor Zimin <ezimine@gmail.com>
2024-07-26 16:47:42 +0700

    update even.py

---
+++ brain_games/games/even.py
@@ -0,0 +1,15 @@
+from random import randint
+
+description = "Answer 'yes' if the number is even, otherwise answer 'no'."
+
+
+def is_even():
+    question = randint(1, 150)
+    result = ''
+
+    if question % 2 == 0:
+        result = 'yes'
+    else:
+        result = 'no'
+
+    return question, result
