3a2d90707504528bfde99c55209ff5043e594eb7
Egor Zimin <ezimine@gmail.com>
2024-07-26 16:44:40 +0700

    add engine.py

---
+++ brain_games/engine.py
@@ -0,0 +1,29 @@
+from brain_games.cli import welcome_user
+import prompt  # type: ignore
+
+
+def play_game(description, game):
+    score = 0
+    name = welcome_user()
+    print(description)
+    question, result = game()
+    print(f'Question: {question}')
+    user_answer = prompt.string('Your answer: ').lower()
+
+    while score < 3:
+        if result == user_answer:
+            print('Correct!')
+            score += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  + f"Correct answer was '{result}'. let's try again, {name}")
+            break
+
+        question, result = game()
+
+        if score == 3:
+            print(f'Congratulations, {name}!')
+            return
+
+        print(question)
+        user_answer = prompt.string('Your answer: ').lower()
