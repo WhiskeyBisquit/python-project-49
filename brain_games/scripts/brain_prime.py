f85ae47daddc652257eca811f4c2fcd48915ac13
Egor Zimin <ezimine@gmail.com>
2024-07-28 11:20:59 +0700

    update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -3,12 +3,8 @@ from brain_games.engine import play_game
 from brain_games.games.prime import description, is_prime
 
 
-def start_prime():
-    play_game(description, is_prime)
-
-
 def main():
-    start_prime()
+    play_game(description, is_prime)
 
 
 if __name__ == '__main__':
