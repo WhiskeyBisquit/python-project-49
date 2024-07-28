69f95137cf2e03aa57c1aa08a52823b921080859
Egor Zimin <ezimine@gmail.com>
2024-07-28 11:19:41 +0700

    update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -3,12 +3,8 @@ from brain_games.engine import play_game
 from brain_games.games.calc import description, math_problem
 
 
-def start_calc():
-    play_game(description, math_problem)
-
-
 def main():
-    start_calc()
+    play_game(description, math_problem)
 
 
 if __name__ == '__main__':
