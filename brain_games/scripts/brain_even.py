2fd7cc1dceb9f558e6c5b9f7ae72c118fe23aaf5
Egor Zimin <ezimine@gmail.com>
2024-07-28 11:20:05 +0700

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,12 +3,8 @@ from brain_games.engine import play_game
 from brain_games.games.even import description, is_even
 
 
-def start_even():
-    play_game(description, is_even)
-
-
 def main():
-    start_even()
+    play_game(description, is_even)
 
 
 if __name__ == '__main__':
