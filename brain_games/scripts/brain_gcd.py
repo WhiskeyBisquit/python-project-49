51a0b742fb3481861c5773ed9417f5caba7d77e6
Egor Zimin <ezimine@gmail.com>
2024-07-28 11:20:33 +0700

    update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -3,12 +3,8 @@ from brain_games.engine import play_game
 from brain_games.games.gcd import description, find_gcd
 
 
-def start_gcd():
-    play_game(description, find_gcd)
-
-
 def main():
-    start_gcd()
+    play_game(description, find_gcd)
 
 
 if __name__ == '__main__':
